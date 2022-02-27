b86e4f25bdf1561f40bd73c6dc1a11884986a83a
IsaDevs <isabel.calzadilla.18@gmail.com>
2022-02-27 14:09:16 +0100

    ejercicio2 completed

---
+++ Empleados/src/main/java/com/example/demo/controller/EmpleadoController.java
@@ -0,0 +1,64 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.Empleado;
+import com.example.demo.services.EmpleadoServices;
+
+/*
+ * @author: Isabel Calzadilla
+ * UD25  REST-SPRING-API
+ * Ejercicio 2 : Empleados
+ * */
+@RestController
+@RequestMapping("/api")
+public class EmpleadoController {
+
+	@Autowired
+	EmpleadoServices empleSERV;
+	
+	@GetMapping("/empleados")
+	public List<Empleado> listaEmpleados(){
+		return empleSERV.totalEmpleados();
+	}
+	
+	@PostMapping("/empleados/add")
+	public Empleado guardaEmpleado(@RequestBody Empleado emple) {
+		return empleSERV.guardaEmpleado(emple);
+	}
+	
+	@GetMapping("/empleados/{id}")
+	public Empleado ubicaPorID(@PathVariable (name = "dni") String dni) {
+		return empleSERV.ubicaPorID(dni);
+	}
+	
+	@PutMapping("/empleados/{id}/actualiza")
+	public Empleado actualizaEmpleado(@PathVariable (name = "dni") String dni,Empleado emple) {
+		Empleado empleSEL = new Empleado();
+		Empleado empleACT = new Empleado();
+		
+		empleSEL = empleSERV.ubicaPorID(dni);
+		empleSEL.setNombre(emple.getNombre());
+		empleSEL.setApellidos(emple.getApellidos());
+		empleSEL.setDepartamento(emple.getDepartamento());
+		
+		empleACT = empleSEL;
+		
+		return empleACT;
+	}
+	
+	@DeleteMapping("/empleados/{id}/elimina")
+	public void eliminaEmpleado(@PathVariable (name = "dni") String dni) {
+		empleSERV.eliminaEmpleado(dni);
+	}
+}
